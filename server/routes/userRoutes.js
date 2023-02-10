99acdadfa1b87621745f5d166f6a0e4a5e11d1ca
Vader1970 <daniel.wilkey@gmail.com>
2023-02-10 18:57:04 +1300

    Profile Screen & Cart Icon

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import User from "../models/User.js";
+
 import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
+import { protectRoute } from "../middleware/authMiddleware.js";
 
 const userRoutes = express.Router();
 
@@ -21,9 +23,11 @@ const loginUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: genToken(user._id),
+      createdAt: user.createdAt,
     });
   } else {
-    res.status(401).json("Invalid email or password.");
+    res.status(401).send("Invalid Email or Password");
+    throw new Error("User not found.");
   }
 });
 
@@ -33,8 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email });
   if (userExists) {
-    res.status(400);
-    throw new Error("We already have an account with that email address.");
+    res.status(400).send("We already have an account with that email address.");
   }
 
   const user = await User.create({
@@ -50,14 +53,42 @@ const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: genToken(user._id),
+      createdAt: user.createdAt,
     });
   } else {
-    res.json(400);
-    throw new Error("Invalid user data");
+    res.status(400).send("We could not register you.");
+    throw new Error("Something went wrong. Please check your data and try again.");
+  }
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: genToken(updatedUser._id),
+      createdAt: updatedUser.createdAt,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found.");
   }
 });
 
 userRoutes.route("/login").post(loginUser);
 userRoutes.route("/register").post(registerUser);
+userRoutes.route("/profile/:id").put(protectRoute, updateUserProfile);
 
 export default userRoutes;
